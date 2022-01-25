d0d532466618d1696e4590209d2c8ba4ac08d97c
mouss <mousseaux.v@gmail.com>
2022-01-25 15:26:38 +0100

    Début TDE_hmm

--- myScripts/myScripts.py
+++ myScripts/myScripts.py
@@ -11,7 +11,32 @@ sys.path.append(r"D:\centrale\3A\info\HMM\myHmmPackage")
 from myHmmPackage.cluster_decoding import *
 from myHmmPackage.cluster_decoder import *
 from sklearn.utils.estimator_checks import check_estimator
+from myHmmPackage.tde_hmm import *
 
+
+#Le dossier "data" contenant les données doit se trouver dans le dossier mère
+
+subj=2
+IC=1
+
+dirname = os.path.dirname(__file__ + "/../../")
+filename = dirname + f"/data/su{subj}IC{IC}_rawdata.nc"
+filename2 = dirname + f"/data/su{subj}IC{IC + 2}_rawdata.nc"
+ds = xr.open_dataset(filename)
+ds2 = xr.open_dataset(filename2)
+X1 = np.transpose(ds['timecourse'].values)[:,:,np.newaxis]
+X2 = np.transpose(ds2['timecourse'].values)[:,:,np.newaxis]
+
+X = np.concatenate((X1, X2), axis=2)
+
+# trialinfo = ds['trialinfo']
+# y = ((trialinfo/10000).astype(int) == 1)
+# [ttrial, N, p] = np.shape(X)
+# Y = np.ones((ttrial, N, 1))
+# for i in range(ttrial):
+#     Y[i]=y
+#
+# X = np.reshape(X, (ttrial*N, p))
 
 TEST = "cluster_decoder"
 
@@ -84,3 +109,33 @@ elif TEST == "cluster_decoder":
     decoder.fit(X,y)
     plt.imshow(decoder.gamma_.T, aspect='auto')
     plt.show()
+
+elif TEST == "tde_hmm":
+    # On récupère un signal
+    X = X[:, :, 0]
+
+    # On concatène pour obtenir qu'un seul array
+    X = np.reshape(X, X.shape[0] * X.shape[1])
+    X = np.reshape(X, -1)
+
+    # On découpe le signal en plusieurs sous signaux décalés d'un cran
+    n_fenetre = 10
+    n = len(X)
+    downsamp_rate = 3
+    signal_crante = np.ones((n-n_fenetre, n_fenetre))
+
+    for i in range(n-n_fenetre):
+        signal_crante[i] = X[i:i+n_fenetre]
+
+    # signal_crante = np.reshape(signal_crante, (-1, 1))
+
+    n_states = 3
+    n_iter = 1000000
+    covariance_type = 'full'
+    tol = 0.01
+    hmm = TDE_HMM(n_components=n_states, n_iter=n_iter, covariance_type=covariance_type, tol=tol)
+    hmm.fit(signal_crante)
+    print(hmm.predict_proba(signal_crante))
+
+    print('FIN')
+
