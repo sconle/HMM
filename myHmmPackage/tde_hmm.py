e34dab041f4b97d3cc3c239411b5c8aaf264cbf4
sconle <59578327+sconle@users.noreply.github.com>
2022-03-23 01:59:27 +0100

    Commentaire sur TDE_HMM

--- myHmmPackage/tde_hmm.py
+++ myHmmPackage/tde_hmm.py
@@ -9,26 +9,27 @@ class TDE_HMM(GaussianHMM):
     TDE_HMM is an Estimator that performs supervised decoding with a predefined number of decoding matrices.
     A clustering method is used to choose which decoding matrix to use for each sample of each input data.
 
-    Attributes
-    ----------
-     n_features : int
-        Dimensionality of the Gaussian emissions.
-    monitor_ : ConvergenceMonitor
-        Monitor object used to check the convergence of EM.
-    startprob_ : array, shape (n_components, )
-        Initial state occupation distribution.
-    transmat_ : array, shape (n_components, n_components)
-        Matrix of transition probabilities between states.
-    means_ : array, shape (n_components, n_features)
-        Mean parameters for each state.
-    covars_ : array
-        Covariance parameters for each state.
-        The shape depends on :attr:`covariance_type`:
-        * (n_components, )                        if "spherical",
-        * (n_components, n_features)              if "diag",
-        * (n_components, n_features, n_features)  if "full",
-        * (n_features, n_features)                if "tied".
-
+    :param int n_components: Number of states. Default 3
+    :param int n_iter: Optional. Maximum number of iterations to perform.
+    :param str covariance_type: Optional. The type of covariance parameters to use:
+            * "spherical" --- each state uses a single variance value that
+              applies to all features (default).
+            * "diag" --- each state uses a diagonal covariance matrix.
+            * "full" --- each state uses a full (i.e. unrestricted)
+              covariance matrix.
+            * "tied" --- all states use **the same** full covariance matrix.
+    :param float min_covar: Optional. Floor on the diagonal of the covariance matrix to prevent overfitting. Defaults to 1e-3.
+    :param array startprob_prior: Optional. Shape of (n_components, ). Parameters of the Dirichlet prior distribution for :attr:`startprob_`.
+    :param array transmat_prior: Optional. Shape of (n_components, n_components). Parameters of the Dirichlet prior distribution for each row of the transition probabilities :attr:`transmat_`.
+    :param array means_prior/means_weight: Optional. Shape of (n_components, ). Mean and precision of the Normal prior distribtion for :attr:`means_`.
+    :param array covars_prior/covars_weight: Optional. Shape of (n_components, ). Parameters of the prior distribution for the covariance matrix :attr:`covars_`.
+            If :attr:`covariance_type` is "spherical" or "diag" the prior is the inverse gamma distribution, otherwise --- the inverse Wishart distribution.
+    :param str algorithm: Optional. {"viterbi", "map"} Decoder algorithm.
+    :param int_seed random_state: Optional. A random number generator instance.
+    :param float tol: Optional. Convergence threshold. EM will stop if the gain in log-likelihood is below this value.
+    :param bool verbose: Optional. Whether per-iteration convergence reports are printed to :data:`sys.stderr`.  Convergence can also be diagnosed using the :attr:`monitor_` attribute.
+    :param str params/init_params: Optional. The parameters that get updated during (``params``) or initialized before (``init_params``) the training.  Can contain any combination of 's' for startprob, 't' for transmat, 'm' for means, and 'c' for covars.  Defaults to all parameters.
+    :param str implementation: Optional. Determines if the forward-backward algorithm is implemented with logarithms ("log"), or using scaling ("scaling").  The default is to use logarithms for backwards compatability.
     """
 
     def __init__(
@@ -42,60 +43,7 @@ class TDE_HMM(GaussianHMM):
             n_iter=10, tol=1e-2, verbose=False,
             params="stmc", init_params="stmc",
     ):
-        """
-            Parameters
-    ----------
-    n_components : int
-            Number of states.
-        covariance_type : {"sperical", "diag", "full", "tied"}, optional
-            The type of covariance parameters to use:
-            * "spherical" --- each state uses a single variance value that
-              applies to all features (default).
-            * "diag" --- each state uses a diagonal covariance matrix.
-            * "full" --- each state uses a full (i.e. unrestricted)
-              covariance matrix.
-            * "tied" --- all states use **the same** full covariance matrix.
-        min_covar : float, optional
-            Floor on the diagonal of the covariance matrix to prevent
-            overfitting. Defaults to 1e-3.
-        startprob_prior : array, shape (n_components, ), optional
-            Parameters of the Dirichlet prior distribution for
-            :attr:`startprob_`.
-        transmat_prior : array, shape (n_components, n_components), optional
-            Parameters of the Dirichlet prior distribution for each row
-            of the transition probabilities :attr:`transmat_`.
-        means_prior, means_weight : array, shape (n_components, ), optional
-            Mean and precision of the Normal prior distribtion for
-            :attr:`means_`.
-        covars_prior, covars_weight : array, shape (n_components, ), optional
-            Parameters of the prior distribution for the covariance matrix
-            :attr:`covars_`.
-            If :attr:`covariance_type` is "spherical" or "diag" the prior is
-            the inverse gamma distribution, otherwise --- the inverse Wishart
-            distribution.
-        algorithm : {"viterbi", "map"}, optional
-            Decoder algorithm.
-        random_state: RandomState or an int seed, optional
-            A random number generator instance.
-        n_iter : int, optional
-            Maximum number of iterations to perform.
-        tol : float, optional
-            Convergence threshold. EM will stop if the gain in log-likelihood
-            is below this value.
-        verbose : bool, optional
-            Whether per-iteration convergence reports are printed to
-            :data:`sys.stderr`.  Convergence can also be diagnosed using the
-            :attr:`monitor_` attribute.
-        params, init_params : string, optional
-            The parameters that get updated during (``params``) or initialized
-            before (``init_params``) the training.  Can contain any combination
-            of 's' for startprob, 't' for transmat, 'm' for means, and 'c' for
-            covars.  Defaults to all parameters.
-        implementation: string, optional
-            Determines if the forward-backward algorithm is implemented with
-            logarithms ("log"), or using scaling ("scaling").  The default is
-            to use logarithms for backwards compatability.
-        """
+
 
         super().__init__(n_components=n_components,
                          covariance_type=covariance_type, min_covar=min_covar,
@@ -107,12 +55,29 @@ class TDE_HMM(GaussianHMM):
                          params=params, init_params=init_params)
 
     def fit(self, X, y=None):
+        """
+        Estimate model parameters.
+
+
+        :param array-like X: The training input samples
+        :param array-like y: The target values
+
+        :returns: Returns self
+        """
+
         X = self.__signal_crante(X)
         super().fit(X)
 
         return self
 
     def predict_proba(self, X, y=None):
+        """
+        Find most likely state sequence corresponding to X, then computes y_predict, the predicted labels given X.
+
+        :param X: The training input samples
+        :returns: Returns posteriors, the predicted labels in an tensor
+        """
+
         n_samples, n_time_points, _ = X.shape
         n_fenetre = 20
         X = self.__signal_crante(X, n_fenetre)
